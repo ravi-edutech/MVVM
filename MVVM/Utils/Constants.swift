971ccf96248fbc06aedc9be9b75424dd84e7913d
Ravi Kumar <ravi.edu.81286@gmail.com>
2025-12-04 02:32:59 +0530

    renamed the schema

--- MVVM/Utils/Constants.swift
+++ MVVM/Utils/Constants.swift
@@ -14,7 +14,7 @@ struct Constants {
     static let sslCertificateName = "dummyjson.com"
     
     struct URLConstants {
-    //    static let baseURL = "https://jsonplaceholder.typicode.com/"
+    //  static let baseURL = "https://jsonplaceholder.typicode.com/"
         static let baseURL = "https://dummyjson.com/"
         static let users = "users"
         static let posts = "posts"
